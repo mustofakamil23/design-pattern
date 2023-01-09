3ddbcd632891d6a746061cf1e29b5cc9b41d4c20
mustofakamil <kamilmustofa23@yahoo.com>
2023-01-09 14:45:19 +0800

    last edit

--- singleton-pattern/singleton.py
+++ singleton-pattern/singleton.py
@@ -10,14 +10,23 @@ class Singleton:
             raise Exception("We can not create another class")
 
         
-        @staticmethod
-        def get_instance():
-            # we defined a static method to fetch instance
-            if not Singleton.__instance__:
-                Singleton()
-            return Singleton.__instance__
+    @staticmethod
+    def get_instance():
+        # we defined a static method to fetch instance
+        if not Singleton.__instance__:
+            Singleton()
+        return Singleton.__instance__
         
 
 # Creating an object
 single = Singleton()
 print(single)
+
+same_single = Singleton.get_instance()
+print(same_single)
+
+another_single = Singleton.get_instance()
+print(another_single)
+
+new_sinle = Singleton()
+print(new_sinle)
